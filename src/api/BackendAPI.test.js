ee0fd0678d8d4b380a1fbea5f5996f4fe00c1b03
Astrid Yu <astrid@astrid.tech>
2021-02-15 16:03:18 -0800

    Update test to match new method name

--- src/api/BackendAPI.test.js
+++ src/api/BackendAPI.test.js
@@ -2,6 +2,5 @@ import BackendAPI from './BackendAPI';
 
 test('gets weather from backend', async () => {
   const api = new BackendAPI('http://localhost:5000');
-  const data = await api.getCurrentWeather(0, 0);
-  console.log(data);
+  await api.getCurrentWeatherFromLocation(0, 0);
 });
