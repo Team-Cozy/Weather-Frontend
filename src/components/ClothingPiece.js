7646b5eda6abfd330a2e1dde1abd735adb8148be
Astrid Yu <astrid@astrid.tech>
2021-03-08 10:55:52 -0800

    Break outfit into multiple files

---
+++ src/components/ClothingPiece.js
@@ -0,0 +1,183 @@
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  IconButton,
+  ListItemAvatar,
+  ListItemText,
+  makeStyles,
+  Tooltip
+} from '@material-ui/core';
+import { Create } from '@material-ui/icons';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useBackendAPI } from 'src/components/BackendAPIProvider';
+import {
+  getUnitConverterByKey,
+  useUnitConverters
+} from 'src/components/UnitConversionProvider';
+import { mapSliderValues } from '../api/utils';
+import { useLoggedInUser } from './UserProvider';
+import ZoneAdjustmentSlider from './ZoneAdjustmentSlider';
+
+const useStyles = makeStyles({
+  image: {
+    height: 48,
+    width: 48
+  }
+});
+
+function SliderEditDialog({
+  open,
+  handleClose,
+  clothingType,
+  onUpdate = () => { }
+}) {
+  const { api } = useBackendAPI();
+  const [saving, setSaving] = useState(false);
+  const [slider, setSlider] = useState(null);
+  const converters = useUnitConverters();
+  const converter = slider == null
+    ? null
+    : getUnitConverterByKey(converters, slider.parameter);
+
+  const profileIndex = 0;
+
+  // TODO get the data in a MUCH better way
+  useEffect(() => {
+    if (!open) return;
+    if (slider != null) return;
+
+    api.getProfile(profileIndex).then((profile) => {
+      const origSlider = profile.sliders[clothingType];
+      console.log('Got slider', profile);
+      setSlider(origSlider);
+    });
+  }, [slider, open]);
+
+  // Saving side effect
+  useEffect(() => {
+    if (!saving) return;
+
+    (async () => {
+      // Get the current profile data (may have changed)
+      const profile = await api.getProfile(profileIndex);
+
+      // Patch the slider
+      profile.sliders[clothingType] = slider;
+      console.log(profile);
+
+      // Send the data back to the backend
+      await api.updateProfile(profileIndex, profile);
+
+      // Close this modal
+      setSaving(false);
+      handleClose();
+      console.log('updated');
+      onUpdate();
+    })();
+  }, [saving]);
+
+  const getConvertedSlider = () => mapSliderValues(slider, converter.convert);
+  const setConvertedSlider = (newSlider) => {
+    setSlider(mapSliderValues(newSlider, converter.convertBack));
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogContent style={{ width: 500 }}>
+        <DialogContentText>
+          Edit
+          {' '}
+          {clothingType}
+          {' '}
+          values
+        </DialogContentText>
+        {
+          slider == null
+            ? (
+              <CircularProgress />
+            )
+            : (
+              <>
+                <ZoneAdjustmentSlider
+                  domain={converter.sliderDomain}
+                  preference={getConvertedSlider()}
+                  setPreference={setConvertedSlider}
+                />
+              </>
+            )
+        }
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={() => setSaving(true)} color="primary">
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+SliderEditDialog.propTypes = {
+  clothingType: PropTypes.string,
+  handleClose: PropTypes.func,
+  onUpdate: PropTypes.func,
+  open: PropTypes.bool
+};
+
+export default function ClothingPiece({ piece, onUpdate = () => { } }) {
+  const classes = useStyles();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const { user } = useLoggedInUser();
+  const loggedIn = user == null || !user.anonymous;
+
+  const handleClose = () => setDialogOpen(false);
+  const editButton = (
+    <IconButton
+      edge="end"
+      size="small"
+      disabled={!loggedIn}
+      onClick={() => {
+        if (loggedIn) setDialogOpen(true);
+      }}
+    >
+      <Create title="Edit" />
+    </IconButton>
+  );
+
+  return (
+    <>
+      <ListItemAvatar>
+        <img alt={piece.name} className={classes.image} src={piece.image} />
+      </ListItemAvatar>
+      <ListItemText primary={piece.name} />
+      <Tooltip
+        title={
+          loggedIn
+            ? 'Change your preferences'
+            : 'Log in to set your preferences!'
+        }
+      >
+        {editButton}
+      </Tooltip>
+
+      <SliderEditDialog
+        clothingType={piece.type}
+        open={dialogOpen}
+        handleClose={handleClose}
+        onUpdate={onUpdate}
+      />
+    </>
+  );
+}
+
+ClothingPiece.propTypes = {
+  piece: PropTypes.object,
+  onUpdate: PropTypes.func
+};
--- src/views/reports/DashboardView/Outfit.js
+++ src/views/reports/DashboardView/Outfit.js
@@ -1,195 +1,26 @@
 import {
-  Button,
   Card,
   CardContent,
   CardHeader,
   CircularProgress,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
   Divider,
-  IconButton,
   List,
   ListItem,
-  ListItemAvatar,
-  ListItemText,
   makeStyles,
-  Tooltip,
   Typography
 } from '@material-ui/core';
-import { Create } from '@material-ui/icons';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { useBackendAPI } from 'src/components/BackendAPIProvider';
-import { getUnitConverterByKey, useUnitConverters } from 'src/components/UnitConversionProvider';
 import { useUserLocation } from 'src/components/UserLocationProvider';
-import { mapSliderValues } from '../../../api/utils';
-import { useLoggedInUser } from '../../../components/UserProvider';
-import ZoneAdjustmentSlider from '../../../components/ZoneAdjustmentSlider';
+import ClothingPiece from '../../../components/ClothingPiece';
 
-const useStyles = makeStyles(({
+const useStyles = makeStyles({
   root: {
     height: '100%'
-  },
-  image: {
-    height: 48,
-    width: 48
   }
-}));
-
-function SliderEditDialog({
-  open, handleClose, clothingType, onUpdate = () => { }
-}) {
-  const { api } = useBackendAPI();
-  const [saving, setSaving] = useState(false);
-  const [slider, setSlider] = useState(null);
-  const converters = useUnitConverters();
-  const converter = slider == null ? null : getUnitConverterByKey(converters, slider.parameter);
-
-  const profileIndex = 0;
-
-  // TODO get the data in a MUCH better way
-  useEffect(() => {
-    if (!open) return;
-    if (slider != null) return;
-
-    api.getProfile(profileIndex).then((profile) => {
-      const origSlider = profile.sliders[clothingType];
-      console.log('Got slider', profile);
-      setSlider(origSlider);
-    });
-  }, [slider, open]);
-
-  // Saving side effect
-  useEffect(() => {
-    if (!saving) return;
-
-    (async () => {
-      // Get the current profile data (may have changed)
-      const profile = await api.getProfile(profileIndex);
-
-      // Patch the slider
-      profile.sliders[clothingType] = slider;
-      console.log(profile);
-
-      // Send the data back to the backend
-      await api.updateProfile(profileIndex, profile);
-
-      // Close this modal
-      setSaving(false);
-      handleClose();
-      console.log('updated');
-      onUpdate();
-    })();
-  }, [saving]);
-
-  const getConvertedSlider = () => mapSliderValues(slider, converter.convert);
-  const setConvertedSlider = (newSlider) => {
-    setSlider(mapSliderValues(newSlider, converter.convertBack));
-  };
-
-  return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogContent style={{ width: 500 }}>
-        <DialogContentText>
-          Edit
-          {' '}
-          {clothingType}
-          {' '}
-          values
-        </DialogContentText>
-        {(slider == null)
-          ? <CircularProgress />
-          : (
-            <>
-              <ZoneAdjustmentSlider
-                domain={converter.sliderDomain}
-                preference={getConvertedSlider()}
-                setPreference={setConvertedSlider}
-              />
-            </>
-          )}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={() => setSaving(true)} color="primary">
-          Save
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-}
-
-SliderEditDialog.propTypes = {
-  clothingType: PropTypes.string,
-  handleClose: PropTypes.func,
-  onUpdate: PropTypes.func,
-  open: PropTypes.bool
-};
-
-function ClothingPiece({
-  piece,
-  onUpdate = () => { }
-}) {
-  const classes = useStyles();
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const { user } = useLoggedInUser();
-  const loggedIn = user == null || !user.anonymous;
-
-  const handleClose = () => setDialogOpen(false);
-  const editButton = (
-    <IconButton
-      edge="end"
-      size="small"
-      disabled={!loggedIn}
-      onClick={() => {
-        if (loggedIn) setDialogOpen(true);
-      }}
-    >
-      <Create title="Edit" />
-    </IconButton>
-  );
-
-  return (
-    <>
-      <ListItemAvatar>
-        <img
-          alt={piece.name}
-          className={classes.image}
-          src={piece.image}
-        />
-      </ListItemAvatar>
-      <ListItemText
-        primary={piece.name}
-      />
-      <Tooltip
-        title={
-          loggedIn
-            ? 'Change your preferences'
-            : 'Log in to set your preferences!'
-        }
-      >
-        {editButton}
-      </Tooltip>
-
-      <SliderEditDialog
-        clothingType={piece.type}
-        open={dialogOpen}
-        handleClose={handleClose}
-        onUpdate={onUpdate}
-      />
-    </>
-  );
-}
-
-ClothingPiece.propTypes = {
-  piece: PropTypes.object,
-  onUpdate: PropTypes.func,
-};
+});
 
 function Outfit({ outfit, onUpdate }) {
   const { pieces } = outfit;
@@ -197,10 +28,7 @@ function Outfit({ outfit, onUpdate }) {
   return (
     <List>
       {Object.entries(pieces).map(([, piece], i) => (
-        <ListItem
-          divider={i < pieces.length - 1}
-          key={piece.id}
-        >
+        <ListItem divider={i < pieces.length - 1} key={piece.id}>
           <ClothingPiece onUpdate={onUpdate} piece={piece} />
         </ListItem>
       ))}
@@ -234,19 +62,16 @@ function OutfitCard({ className, ...rest }) {
     if (location == null) return;
 
     console.log('fetching outfit');
-    api.getOutfit(location)
-      .then((response) => {
-        setOutfit(response);
-      });
+    api.getOutfit(location).then((response) => {
+      setOutfit(response);
+    });
   }, [api, location, outfit]);
 
   let content;
   if (location == null) {
     content = (
       <CardContent>
-        <Typography>
-          Please choose a location first.
-        </Typography>
+        <Typography>Please choose a location first.</Typography>
       </CardContent>
     );
   } else if (outfit == null) {
@@ -260,13 +85,8 @@ function OutfitCard({ className, ...rest }) {
   }
 
   return (
-    <Card
-      className={clsx(classes.root, className)}
-      {...rest}
-    >
-      <CardHeader
-        title="Outfit Suggestion!"
-      />
+    <Card className={clsx(classes.root, className)} {...rest}>
+      <CardHeader title="Outfit Suggestion!" />
       <Divider />
       {content}
     </Card>
